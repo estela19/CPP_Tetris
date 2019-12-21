cddcd2a058388e47f1e758c05762b9baecdcc8ff
estela19 <e9857256@naver.com>
2019-12-22 04:58:14 +0900

    Feat : Create main

---
+++ Sources/Tetris/main.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+
+#include<Tetris/Board.h>
+#include<Tetris/Game.h>
+#include<Tetris/screen.h>
+
+using namespace Tetris;
+
+int main() {
+    Screen s;
+    s.PrintBoard();
+    return 0;
+}
