e5ac28ca718ee545804447648de28b85055d41b7
estela19 <e9857256@naver.com>
2019-12-24 03:32:23 +0900

    Feat(board) : Create board methods

--- Includes/Tetris/Board.h
+++ Includes/Tetris/Board.h
@@ -1,24 +1,31 @@
 #ifndef _TETRIS_BOARD_H_
 #define _TETRIS_BOARD_H_
 
-#include <Tetris/game.h>
+#include <Tetris/Game.h>
 
 #include <array>
+#include <set>
 
 namespace Tetris
 {
+using Boardtype = std::array<std::array<int, 20>, 10>;
+
+class Game;
 class Board
 {
-    using Boardtype = std::array<std::array<int, 10>, 20>;
-
  public:
     Boardtype& GetBoard();
     bool IsFullRow(std::size_t idx);
-    bool HasFullRow();
+    void GetFullRow();
+    void UpdateBoard();
+    void ClearBoard();
+    void ClearLine();
+    void DeleteLine(std::size_t idx);
 
  private:
     Game* game;
     Boardtype board;
+    std::set<std::size_t> fullIdx;
 };
 }  // namespace Tetris
 
--- Sources/Tetris/Board.cc
+++ Sources/Tetris/Board.cc
@@ -7,8 +7,52 @@ Boardtype& Board::GetBoard()
     return board;
 }
 
-bool Board::IsFullRow()
+bool Board::IsFullRow(std::size_t idx)
 {
+    bool isfull = true;
+    for (std::size_t i = 0; i < game->GetWidth(); i++)
+    {
+        if (board[idx][i] == 0)
+        {
+            isfull = false;
+            break;
+        }
+    }
+    return isfull;
+}
 
+void Board::GetFullRow()
+{
+    for (std::size_t i = 0; i < game->GetHeight(); i++)
+    {
+        if (IsFullRow(i) == true)
+        {
+            fullIdx.insert(i);
+        }
+    }
+}
+
+void Board::UpdateBoard()
+{
+    for (auto i = fullIdx.rbegin(); i != fullIdx.rend(); i++)
+    {
+        std::size_t full = *i;
+        for (std::size_t j = full; j > 0; j--)
+        {
+        }
+    }
+}
+
+void Board::ClearBoard()
+{
+    for (int i = 0; i < game->GetHeight(); i++)
+    {
+      //  board[i].clear();
+    }
+}
+
+void Board::DeleteLine(std::size_t idx)
+{
+    board[idx].fill(0);
 }
 }  // namespace Tetris
