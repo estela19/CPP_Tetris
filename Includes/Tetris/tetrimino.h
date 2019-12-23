aab612dbc07b3f598525df28cae45bce62d07d76
estela19 <e9857256@naver.com>
2019-12-24 01:19:16 +0900

    Feat(tetrimino) : Create tetrimino class

---
+++ Includes/Tetris/tetrimino.h
@@ -0,0 +1,40 @@
+#ifndef _TETRIS_TETRIMINO_H_
+#define _TETRIS_TETRIMINO_H_
+
+#include <Tetris/Board.h>
+#include <Tetris/Point.h>
+#include <Tetris/Enums.h>
+#include <Tetris/Pattern.h>
+#include <Tetris/Game.h>
+
+namespace Tetris
+{
+class Tetrimino
+{
+ public:
+    Game& GetGame();
+    Board& GetBoard();
+    void MoveDown();
+    void MoveLeft();
+    void MoveRight();
+    void GoFloor();
+    void Rotate();
+    
+    void SetType();
+    Point GetPos();
+    TetriminoType GetTType();
+    RotateType GetRType();
+
+
+ private:
+    Game* game;
+    Board* board;
+    const Point* type;
+    TetriminoType Ttype;
+    RotateType Rtype;
+    bool isfloor = false;
+    Point Pos;
+};
+}  // namespace Tetris
+
+#endif  // !_TETRIS_TETRIMINO_H_
---
+++ Sources/Tetris/Tetrimino.cc
@@ -0,0 +1,67 @@
+#include <Tetris/tetrimino.h>
+
+namespace Tetris
+{
+Game& Tetrimino::GetGame()
+{
+    return *game;
+}
+
+Board& Tetrimino::GetBoard()
+{
+    return *board;
+}
+
+void Tetrimino::MoveDown()
+{
+}
+
+void Tetrimino::MoveLeft()
+{
+    std::size_t& minX = game->GetOriginX();
+
+    if (Pos.GetX() > minX + 1)
+    {
+        Pos.SetX(Pos.GetX() - 1);
+    }
+}
+
+void Tetrimino::MoveRight()
+{
+    std::size_t& maxX = game->GetOriginX() + game->GetScrWidth();
+
+    if (Pos.GetX() < maxX - 1)
+    {
+        Pos.SetX(Pos.GetX() + 1);
+    }
+}
+
+void Tetrimino::GoFloor()
+{
+}
+
+void Tetrimino::Rotate()
+{
+}
+
+void Tetrimino::SetType()
+{
+    type = &patterns[static_cast<int>(Ttype)][static_cast<int>(Rtype)][0];
+}
+
+Point Tetrimino::GetPos()
+{
+    return Pos;
+}
+
+TetriminoType Tetrimino::GetTType()
+{
+    return Ttype;
+}
+
+RotateType Tetrimino::GetRType()
+{
+    return Rtype;
+}
+
+}  // namespace Tetris
